34a9bd8fa5ec26e881b76cb1efadff4523d7059a
QuantumGhost <obelisk.reg@gmail.com>
2013-06-13 20:35:37 +0800

    rewritten with requests
    rewritten with Requests

--- pysrun3000/srun3000.py
+++ pysrun3000/srun3000.py
@@ -1,48 +1,35 @@
-import pycurl
-import StringIO
-import urllib
+import requests
 
-b = StringIO.StringIO()
-c = pycurl.Curl()
 
-def reset():
-    b.truncate(0)
-    c.reset()
-    c.setopt(pycurl.WRITEFUNCTION, b.write)
-    
+host = 'auth.example.com'   # Authenticatioon Server 
+
+
 def get_mac():
     from uuid import uuid1
-    mac = uuid1().hex[-12:]
-    mac = list(mac)
-    for i in (10, 8, 6, 4, 2):
-        mac.insert(i, ':')
-    return ''.join(mac)
-        
-def login(host, mac, username, password):
-    reset()
-    c.setopt(pycurl.URL, 'http://%s:3333/cgi-bin/do_login' % host)
-    c.setopt(pycurl.POST, True)
-    c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-        'username': username,
-        'password': password,
-        'mac': mac,
-        'n': '99',
-        'type': '3',
-        'drop': '0',
-    }))
-    c.perform()
-    return b.getvalue()
-    
+	mac = uuid1().hex[-12:]
+	mac = list(mac)
+	for i in range(10, 0, -2):
+		mac.insert(i, ':')
+	return ''.join(mac)
+
+
+def login(host, username, password):
+	params = {
+		'n': '100', 'is_pad': '1', 'type': '1',
+		'uname': username, 'pass': password, 'drop': '0',
+		'x': '98', 'y': '24', 'mac': get_mac()
+	}
+	r = requests.post(
+		'http://{}/cgi-bin/do_login'.format(host), params)
+	print(r.text)
+
+
 def logout(host, uid):
-    reset()
-    c.setopt(pycurl.URL, 'http://%s:3333/cgi-bin/do_logout' % host)
-    c.setopt(pycurl.POST, True)
-    c.setopt(pycurl.POSTFIELDS, urllib.urlencode({
-        'uid': uid,
-    }))
-    c.perform()
-    return b.getvalue()
-    
-uid = login('202.112.136.131', get_mac(), 'username', 'password')
-print uid
-#print logout('202.112.136.131', uid)
+	params = {
+		'uid': uid
+	}
+	r = requests.post('http://{}/cgi-bin/do_logout'.format(host), params)
+
+
+uid = login(host, 'username', 'password')
+# logout(host, uid)
